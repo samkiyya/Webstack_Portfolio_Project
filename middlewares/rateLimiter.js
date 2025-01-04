fc147618b1f7156ca5b9a80c6d60bbc40e0e5f55
samkiyya <samuelkiyya2012@gmail.com>
2025-01-04 09:03:28 -0800

    rate limitter middleware to make the system performant

---
+++ middlewares/rateLimiter.js
@@ -0,0 +1,12 @@
+/*const rateLimit = require('express-rate-limit');
+
+const loginRateLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, 
+  max: 10, 
+  message: { error: 'Too many login attempts, please try again later.' }, 
+  statusCode: 429, 
+  headers: true, 
+});
+
+module.exports = loginRateLimiter;
+*/
